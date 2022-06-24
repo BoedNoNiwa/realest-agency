1c89696ab3ba000a497576c5c9ca93ec6ae4f015
BoedNoNiwa <dukunas27@gmail.com>
2022-06-24 16:04:14 +0700

    Bug Fixing

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 import Image from 'next/image'
 import { Box, Icon, Flex } from '@chakra-ui/react'
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
@@ -41,8 +40,8 @@ export default function ImageScrollbar({data}) {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: "hidden"}} >
         {data.map((item, index) => (
-            <Box width="910px" itemId={item.id} overflow="hidden" p="1">
-                <Image key={index} placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
+            <Box key={index} width="910px" itemId={item.id} overflow="hidden" p="1">
+                <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
             </Box>
         ))}
     </ScrollMenu>
